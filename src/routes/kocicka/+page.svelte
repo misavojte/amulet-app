534cd8b406e83d4e30c88677b9a5d8152ab1555f
KhonsuDeMon <marketa.muczkova01@upol.cz>
2023-12-05 18:06:32 +0100

    novy scenar zkouska

---
+++ src/routes/kocicka/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import App from "../../components/App.svelte";
+
+    const gameConfig = {
+        numberOfRounds: 8,
+        startScore: 40,
+        scenario: 'AlwaysLose',
+        priceOfAmulet: 5,
+        scoreOnWin: 20,
+    };
+
+    const locale = 'cs';
+</script>
+
+<App locale={locale} gameConfig={gameConfig} />
