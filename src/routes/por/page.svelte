b34ac3c330a9be6ac6ebe36804029638fc7cc5ca
KhonsuDeMon <marketa.muczkova01@upol.cz>
2023-12-10 23:50:43 +0100

    Polské verze

---
+++ src/routes/por/page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import App from "../../components/App.svelte";
+
+    const gameConfig = {
+        numberOfRounds: 5,
+        startScore: 50,
+        scenario: 'Random',
+        priceOfAmulet: 10,
+        scoreOnWin: 30,
+    };
+
+    const locale = 'pl';
+</script>
+
+<App locale={locale} gameConfig={gameConfig} />
