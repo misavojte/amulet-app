dce8082da1dd6109c5e0e325366a4f1f03e2ad6a
KhonsuDeMon <marketa.muczkova01@upol.cz>
2024-02-27 21:25:31 +0100

    Startin budget adjustment from 110 to 100

--- src/routes/poaw/+page.svelte
+++ src/routes/poaw/+page.svelte
@@ -3,7 +3,7 @@
 
     const gameConfig = {
         numberOfRounds: 11,
-        startScore: 110,
+        startScore: 100,
         scenario: 'AlwaysWin',
         priceOfAmulet: 10,
         scoreOnWin: 30,
--- src/routes/tri-bge/+page.svelte
+++ src/routes/tri-bge/+page.svelte
@@ -3,7 +3,7 @@
 
     const gameConfig = {
         numberOfRounds: 11,
-        startScore: 110,
+        startScore: 100,
         scenario: 'ZigZag',
         priceOfAmulet: 10,
         scoreOnWin: 30,
