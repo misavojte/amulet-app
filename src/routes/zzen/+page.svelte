ba5d5cf1adf66d0effa3f0477f4cbb3591f9d8f1
KhonsuDeMon <marketa.muczkova01@upol.cz>
2024-06-21 11:21:41 +0200

    4 new english routes created

---
+++ src/routes/zzen/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import App from "../../components/App.svelte";
+
+    const gameConfig = {
+        numberOfRounds: 11,
+        startScore: 100,
+        scenario: 'ZigZag',
+        priceOfAmulet: 10,
+        scoreOnWin: 30,
+    };
+
+    const locale = 'en';
+</script>
+
+<App locale={locale} gameConfig={gameConfig} />
