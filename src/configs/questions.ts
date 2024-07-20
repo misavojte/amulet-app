14646efcd9fa1abbe67a4c93cb66ee452388ae7c
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-07-20 11:21:22 +0200

    :sparkles: Add questions configuration file and prepare language packages

---
+++ src/configs/questions.ts
@@ -0,0 +1,295 @@
+export const questions = [
+	{
+		id: 'Q1IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q2IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q3IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q4IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q5IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q6IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q7IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q8IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q9IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q10IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q11IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q12IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q13IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q14IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q15IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q16IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q17IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q18IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q19IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q20IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q21IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q22IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q23IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q24IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q25IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q26IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q27IBI',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q1TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q2TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q3TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q4TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q5TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q6TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q7TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q8TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q9TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q10TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q11TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q12TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q13TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q14TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q15TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q16TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q17TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q18TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q19TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q20TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q21TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q22TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q23TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'Q24TS',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'age',
+		type: 'number'
+	},
+	{
+		id: 'gender',
+		type: 'select',
+		options: ['F', 'M', 'O', 'N']
+	},
+	{
+		id: 'QamuletEffect',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'QstrategyEffect',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'QchanceEffect',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'QlikeExperience',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'QfrustrationLevel',
+		type: 'select',
+		options: ['1', '2', '3', '4', '5']
+	},
+	{
+		id: 'email',
+		type: 'email'
+	}
+] as const;
