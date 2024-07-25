f29956a3e6454438a24c38cf264c8b611228cb64
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-07-25 13:15:40 +0200

    :bug: coin path fix

--- src/components/GameCoin.svelte
+++ src/components/GameCoin.svelte
@@ -1,24 +1,35 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <div>
-    <img src="coin.png" width="120" height="120" alt="coin" class="rotate" />
+	<img src="{base}/coin.png" width="120" height="120" alt="coin" class="rotate" />
 </div>
 
 <style>
-img {
-    animation: spin 2s linear infinite;
-}
+	img {
+		animation: spin 2s linear infinite;
+	}
 
-div {
-    animation: updown 1s ease-in-out infinite alternate
-}
+	div {
+		animation: updown 1s ease-in-out infinite alternate;
+	}
 
-@keyframes spin {
-    0% { transform: rotateY(0deg); }
-    100% { transform: rotateY(360deg); }
-}
+	@keyframes spin {
+		0% {
+			transform: rotateY(0deg);
+		}
+		100% {
+			transform: rotateY(360deg);
+		}
+	}
 
-@keyframes updown {
-    0% { transform: translateY(0px); }
-    100% { transform: translateY(-5px); }
-}
-    
+	@keyframes updown {
+		0% {
+			transform: translateY(0px);
+		}
+		100% {
+			transform: translateY(-5px);
+		}
+	}
 </style>
